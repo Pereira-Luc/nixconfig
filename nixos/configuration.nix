1e7f88de84d650e02b1264817b60d8f72b249390
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-06-13 14:35:17 +0200

    overall formating and adding gaming relevant srtuff

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -25,6 +25,8 @@
   };
 
 
+
+  #################################### awesomeWM config ##############################################
   # Overlays are a way to override attributes of packages. This is often useful
   nixpkgs.overlays = [
     (final: prev: {
@@ -58,8 +60,6 @@
     })
   ];
 
-  
-
   services.xserver.windowManager.awesome = {
     enable = true;
     package = pkgs.awesomeGit;
@@ -85,6 +85,31 @@
       "95:class_g = 'Thunar'"
     ];
   };
+ 
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  #################################### awesomeWM config ##############################################
+  #################################### HyperLand config ##############################################
+  # programs.hyprland = {
+  #   enable = true;
+  #   xwayland.enable = true;
+  # };
+
+  # environment.sessionVariables = {
+  #   WLR_NO_HARDWARE_CURSORS = "1";
+  #   NIXOS_OZONE_WL = "1";
+  # };
+
+  # xdg.portal.enable = true;
+  # xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-gtk ];
+  # services.pipewire = {
+  #   enable = true;
+  #   alsa.enable = true;
+  #   alsa.support32Bit = true;
+  #   pulse.enable = true;
+  #   jack.enable = true;
+  # };
+  ################################### HyperLand config ###############################################
 
   # Enable the Flakes feature and the accompanying new nix command-line tool
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
@@ -96,10 +121,6 @@
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -109,8 +130,7 @@
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
+
 
   # Nvidia Stuff
   # Enable OpenGL
@@ -128,27 +148,10 @@
     # Modesetting is required.
     modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
     powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
-
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
@@ -157,6 +160,14 @@
   
   programs.zsh.enable = true;
   users.defaultUserShell = pkgs.zsh;
+
+  # Gaming Stuff
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession = {
+    enable = true;
+    args = [ "--hdr-enabled" "-W 2560" "-H 1440" "-r 240" "" ];
+  };
+  
 
   # Enable the Deepin Desktop Environment.
   #services.xserver.displayManager.lightdm.enable = true;
@@ -176,19 +187,7 @@
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = true;
   security.rtkit.enable = true;
-  #services.pipewire = {
-  #  enable = true;
-  #  alsa.enable = true;
-  #  alsa.support32Bit = true;
-  #  pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-  #  jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  #};
-
+  
   # Enable touchpad support (enabled default in most desktopManager).
   # services.xserver.libinput.enable = true;
 
@@ -235,6 +234,17 @@
     pavucontrol
     xorg.xbacklight
     openvpn
+    xclip
+    mangohud
+    gamescope-wsi
+
+    gnome.mutter
+
+    # For HyperLand
+    # waybar
+    # dunst
+    # libnotify
+    #rofi-wayland
   ];
 
 
@@ -276,41 +286,11 @@
 
   ];
 
-  # AwesomeWM
-  # windowManager.awesome = {
-  #     enable = true;
-  #     luaModules = with pkgs.luaPackages; [
-  #       luarocks # is the package manager for Lua modules
-  #       luadbi-mysql # Database abstraction layer
-  #     ];
 
-  #   };
+  networking.firewall.enable = true;
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  networking.firewall.allowedUDPPorts = [80 443];
+  networking.firewall.allowedTCPPorts = [80 443];
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
