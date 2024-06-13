fa5971e0cab7d70d155b1946490edf93dd6bf5c8
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-06-13 14:31:22 +0200

    add imports

--- home-manager/programs/configs/nixvim/default.nix
+++ home-manager/programs/configs/nixvim/default.nix
@@ -4,15 +4,26 @@
     ./bufferline.nix
     ./plugins/cmp.nix
     ./plugins/lsp/main.nix
+    ./plugins/lsp/lint.nix
     ./plugins/nvimtree.nix
     ./plugins/treesitter.nix
     ./plugins/telescope.nix
+    ./plugins/other.nix
+    ./plugins/custom.nix
+    
+
+    # Extras
+    ./plugins/extrapkgs.nix
+    ./plugins/indetBlankline.nix
 
     # Mappings
     ./mappings/mappings.nix
 
     #options
     ./options.nix
+
+    # Foemating
+    ./plugins/lsp/format.nix
   ];
 
   colorschemes.dracula.enable = true;
