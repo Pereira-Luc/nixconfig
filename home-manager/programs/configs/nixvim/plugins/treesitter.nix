7b042ff2f6e54da51bbca37935a0bc42d61dc44a
Pereira-Luc <luc.pereira.cardoso@gmail.com>
2024-06-13 14:43:31 +0200

    small change

--- home-manager/programs/configs/nixvim/plugins/treesitter.nix
+++ home-manager/programs/configs/nixvim/plugins/treesitter.nix
@@ -7,7 +7,7 @@
     };
     treesitter-context = {
       enable = true;
-      settings = { max_lines = 2; };
+      settings = { max_lines = 4; };
     };
     rainbow-delimiters.enable = true;
   };
